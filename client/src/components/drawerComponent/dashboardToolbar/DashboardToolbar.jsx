aa155869ee55c82126ff7671e47dbb37c8f450b1
felix.jimenez <felix.jimenez@solera.com>
2023-01-26 11:28:40 +0100

    [Profile] Added user-avatar styling

--- client/src/components/drawerComponent/dashboardToolbar/DashboardToolbar.jsx
+++ client/src/components/drawerComponent/dashboardToolbar/DashboardToolbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './DashboardToolbar.scss';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import MenuIcon from '@mui/icons-material/Menu';
@@ -7,6 +8,9 @@ import MuiAppBar from '@mui/material/AppBar';
 import { styled } from '@mui/material/styles';
 import { Link } from 'react-router-dom';
 
+const user = {
+    avatar: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png'
+};
 
 export default function DashboardToolbar({ drawerWidth, open, handleDrawerOpen }) {
 
@@ -28,20 +32,34 @@ export default function DashboardToolbar({ drawerWidth, open, handleDrawerOpen }
     }));
 
     return (
-        <AppBar position="fixed" open={open}>
+        <AppBar position="fixed" open={open} className='dashboard-toolbar-container'>
             <Toolbar sx={{ backgroundColor: '#fff', color: '#38444c' }}>
-                <IconButton
-                    color="inherit"
-                    aria-label="open drawer"
-                    onClick={handleDrawerOpen}
-                    edge="start"
-                    sx={{ mr: 2, ...(open && { display: 'none' }) }}
-                >
-                    <MenuIcon />
-                </IconButton>
-                <Typography variant="h6" noWrap component="div">
-                    <Link to="/" style={{fontWeight: '700', textDecoration: 'none', color: '#38444c'}}>Inicio</Link>
-                </Typography>
+                <div className="navbar">
+                    <div className="nav-menu">
+                        <IconButton
+                            color="inherit"
+                            aria-label="open drawer"
+                            onClick={handleDrawerOpen}
+                            edge="start"
+                            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" noWrap component="div">
+                            <Link to="/" style={{ fontWeight: '700', textDecoration: 'none', color: '#38444c' }}>Inicio</Link>
+                        </Typography>
+                    </div>
+                    <div className="nav-socials">
+                        {
+                            user.avatar ?
+                                <img className='user-avatar' src={user?.avatar} alt="user-avatar" />
+                                :
+                                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="bi bi-person user-avatar" viewBox="0 0 16 16">
+                                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
+                                </svg>
+                        }
+                    </div>
+                </div>
             </Toolbar>
         </AppBar>
     )
