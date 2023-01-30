c816c683551a11545133acee280fb77311c876c6
felix.jimenez <felix.jimenez@solera.com>
2023-01-30 12:06:26 +0100

    [Demo purposes]

--- client/src/components/welcome/welcomeHeader/WelcomeHeader.jsx
+++ client/src/components/welcome/welcomeHeader/WelcomeHeader.jsx
@@ -7,13 +7,13 @@ export default function WelcomeHeader() {
   return (
     <div className='welcome-header-container'>
       <div className="getting-started">
+        <Link to="dashboard" style={{ textDecoration: 'none' }}><Button variant='contained' className='btn-home' id="btn-info">Tablas</Button></Link>
         <div className="getting-started-title">
           SV-Tech
         </div>
         <div className="getting-started-actions">
           <Link to="register"><button className='btn-home' id="btn-start">Comenzar</button></Link>
           <Link><button className='btn-home' id="btn-info">Más información</button></Link>
-          <Link to="dashboard"><Button variant='filled' className='btn-home' id="btn-info">Tablas</Button></Link>
         </div>
       </div>
     </div>
