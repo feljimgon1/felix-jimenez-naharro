297a8722f53623cb018a2b21c2f78f1fe2449b6c
felix.jimenez <felix.jimenez@solera.com>
2023-02-02 16:16:34 +0100

    [Chart] Incluida gráfica de líneas para tablas previsionales

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -2,15 +2,45 @@ import React from 'react';
 import './Dashboard.scss'
 import { BalanceChart } from '../../components/charts/balance/BalanceChart';
 import { CuentaPerdidasGananciasChart } from '../../components/charts/cuenta-perdidas-ganancias/CuentaPerdidasGananciasChart';
+import { EstrategiaMercadoChart } from '../../components/charts/estrategia-mercado/EstrategiaMercadoChart';
 
 export default function Dashboard() {
   return (
     <div className='dashboard-container'>
-      <div className='situation'>
-        <BalanceChart />
+      <div className="title">Dashboard</div>
+      <div className='situation-container'>
+        <div className="chart-container">
+          <div className="chart-title">Balance</div>
+          <div className="chart">
+            <BalanceChart />
+          </div>
+        </div>
       </div>
-      <div className='previsional'>
-
+      <div className='previsional-container'>
+        <div className="chart-container">
+          <div className="chart-title">Estrategia de mercado</div>
+          <div className="chart">
+            <EstrategiaMercadoChart />
+          </div>
+        </div>
+        <div className="chart-container">
+          <div className="chart-title">Balance</div>
+          <div className="chart">
+            <BalanceChart />
+          </div>
+        </div>
+        <div className="chart-container">
+          <div className="chart-title">Balance</div>
+          <div className="chart">
+            <BalanceChart />
+          </div>
+        </div>
+        <div className="chart-container">
+          <div className="chart-title">Balance</div>
+          <div className="chart">
+            <BalanceChart />
+          </div>
+        </div>
       </div>
     </div>
   )
